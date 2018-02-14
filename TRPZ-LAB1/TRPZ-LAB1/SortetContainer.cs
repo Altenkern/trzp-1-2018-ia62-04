87c7235c7d8fd9610b4178f0349f1d2308a4f830
OblauhovVlad <hameleony2@ukr.net>
2018-02-14 12:06:48 +0200

    Добавлен метод сортировки.

--- TRPZ-LAB1/TRPZ-LAB1/SortetContainer.cs
+++ TRPZ-LAB1/TRPZ-LAB1/SortetContainer.cs
@@ -8,5 +8,20 @@ namespace TRPZ_LAB1
 {
     class SortetContainer
     {
+        internal static void stupidSort(int[] arr1)
+        {
+            int i = 0, tmp;
+            while (i < arr1.Length - 1)
+            {
+                if (arr1[i + 1] < arr1[i])
+                {
+                    tmp = arr1[i];
+                    arr1[i] = arr1[i + 1];
+                    arr1[i + 1] = tmp;
+                    i = 0;
+                }
+                else i++;
+            }
+        }
     }
 }
